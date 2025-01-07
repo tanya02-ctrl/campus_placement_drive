e2ff1f95be7916b37a3f8e5f48c8975af55c5c17
Tanya Shree <129191035+tanya02-ctrl@users.noreply.github.com>
2025-01-07 22:39:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="" type="image/icon">
+        <link rel="icon" href="" type="image/icon">
+        <title>LBSIMDS Home</title>
+        <link rel="stylesheet" href="css/bootstrapmin.css" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+        <script src="js/bootstrap.bundle.js"></script>
+    </head>
+
+    <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background: darkred;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="https://lbsimds.ac.in/" target="_blank">
+      <i class="fas fa-university me-2"></i>LBSIMDS
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="./student/login.php">
+            <i class="fas fa-user-graduate me-2"></i>Student
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./companies/login.php">
+            <i class="fas fa-building me-2"></i>Company
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./admin/login.php">
+            <i class="fas fa-cogs me-2"></i>College
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<header class="bg-dark text-light py-5">
+  <div class="container">
+    <div class="row align-items-center justify-content-center text-center">
+      <div class="col-lg-8 col-md-10">
+
+        <h1 class="display-3  mb-4">Career Placement Drive</h1>
+        <hr class="bg-light mx-auto mb-4" style="width: 50%;">
+
+        <p class="lead mb-4">
+          A Placement is the process of connecting candidates with job opportunities, ensuring efficient recruitment and career development.
+          <br>We are here to build your skills and career with our driven passion and reality.
+        </p>
+
+        <h3 class="text-white mb-4">Click Below To Get Other Options To Apply</h3>
+        <a href="https://offcampusjobs4u.com/" target="_blank" class="btn btn-primary btn-xl">
+          <i class="fas fa-briefcase me-2"></i> Current Opportunities
+        </a>
+      </div>
+    </div>
+  </div>
+</header>
+
+    <?php include 'footer.php' ?>
+    </body>
+
+</html>
