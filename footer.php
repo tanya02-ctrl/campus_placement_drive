6c8cbc5b8a8ebdf9ee63bcfd58d6034d89c88a46
Tanya Shree <129191035+tanya02-ctrl@users.noreply.github.com>
2025-01-07 22:39:22 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,45 @@
+
+<footer class="bg-dark text-light pt-3 pb-1">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-6 mb-4">
+        <h6 class="text-uppercase fw-bold mb-3">LBSIMDS</h6>
+        <p>Empowering future leaders with quality education and training. Join us to make your career goals a reality.</p>
+        <a href="https://lbsimds.ac.in/" class="text-light text-decoration-none">
+          <img src="footer.jpg" alt="LBSIMDS Logo" class="img-fluid" style="height:5rem; width:20rem">
+        </a>
+      </div>
+      <div class="col-lg-2 col-md-6 mb-4">
+        <h6 class="text-uppercase fw-bold mb-3">Quick Links</h6>
+        <ul class="list-unstyled">
+          <li><a href="index.php" class="text-light text-decoration-none"><i class="fas fa-home me-2"></i>Home</a></li>
+          <br><li><a href="./student/login.php" class="text-light text-decoration-none"><i class="fas fa-user me-2"></i>Student</a></li>
+          <br><li><a href="./companies/login.php" class="text-light text-decoration-none"><i class="fas fa-building me-2"></i>Company</a></li>
+          <br><li><a href="./admin/login.php" class="text-light text-decoration-none"><i class="fas fa-cogs me-2"></i>Admin</a></li>
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4">
+        <h6 class="text-uppercase fw-bold mb-3">Get In Touch</h6>
+        <p><i class="fas fa-map-marker-alt me-2"></i> Gaurabagh, Lucknow, Uttar Pradesh, 226026</p>
+        <p><i class="fas fa-phone-alt me-2"></i> 7309299955, 7007077500</p>
+        <p><i class="fas fa-envelope me-2"></i><a href="mailto:info@lbsimds.ac.in" class="text-light text-decoration-none">info@lbsimds.ac.in</a></p>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4">
+        <h6 class="text-uppercase fw-bold mb-3">Follow Us</h6>
+        <p>Stay connected through social media:</p>
+        <a href="https://facebook.com" class="text-light text-decoration-none me-3"><i class="fab fa-facebook fs-4"></i></a>
+        <a href="https://twitter.com" class="text-light text-decoration-none me-3"><i class="fab fa-twitter fs-4"></i></a>
+        <a href="https://instagram.com" class="text-light text-decoration-none me-3"><i class="fab fa-instagram fs-4"></i></a>
+        <a href="https://linkedin.com" class="text-light text-decoration-none"><i class="fab fa-linkedin fs-4"></i></a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 text-center text-md-start">
+        <p>&copy; <?php echo date('Y'); ?> LBSIMDS. All rights reserved.</p>
+      </div>
+      <div class="col-md-6 text-center text-md-end">
+        <p>Designed & Managed By: <strong>Tanya Shree</strong></p>
+      </div>
+    </div>
+  </div>
+</footer>
